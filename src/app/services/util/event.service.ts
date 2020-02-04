0ecd34375f64788a64b6379f270e0c20c7e65533
Daniel Gerhardt <code@dgerhardt.net>
2020-02-04 11:38:34 +0100

    Get rid of RxJS 5 compatibility layer

--- src/app/services/util/event.service.ts
+++ src/app/services/util/event.service.ts
@@ -1,5 +1,4 @@
-import { Subject } from 'rxjs/Subject';
-import { Observable } from 'rxjs/Observable';
+import { Observable, Subject } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 
 interface BroadcastEvent {
