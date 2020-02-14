16041cdd07bf722e43375b0d53db2d7df3d4f322
Lukas Mauß <lukas.mauss@mni.thm.de>
2020-02-14 10:23:23 +0100

    Cleanup assets folder

--- src/assets/i18n/cookies/cookies-de.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-cookies-de',
-  templateUrl: './cookies-de.html',
-  styleUrls: ['./cookies.scss']
-})
-
-export class CookiesDeComponent {}
