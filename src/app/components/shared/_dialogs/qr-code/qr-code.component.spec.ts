1809685e4396f16b5b0fff5dda8e5627cd9649a1
Lukas Mauß <lukas.mauss@mni.thm.de>
2020-02-19 20:39:58 +0100

    Use 'angularx-qrcode' lib for room join qrcode

---
+++ src/app/components/shared/_dialogs/qr-code/qr-code.component.spec.ts
@@ -0,0 +1,27 @@
+/*
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { QrCodeComponent } from './qr-code.component';
+
+describe('QrCodeComponent', () => {
+  let component: QrCodeComponent;
+  let fixture: ComponentFixture<QrCodeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ QrCodeComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QrCodeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+*/
