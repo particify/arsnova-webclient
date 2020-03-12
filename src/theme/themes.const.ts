3529094f47bda5b3b5d18b4f3cfffab47eeb81bc
Lukas Mauß <lukas.mauss@gmx.de>
2020-03-12 23:38:11 +0100

    Remove unused themes and rename main theme

--- src/theme/themes.const.ts
+++ src/theme/themes.const.ts
@@ -1,18 +1,12 @@
 import { dark, dark_meta } from './dark-theme/dark-theme.const';
-import { light, light_meta } from './light-theme/light-theme';
-import { beamer, beamer_meta } from './beamer-theme/beamer-theme.const';
-import { highContrast, highContrast_meta } from './high-contrast-theme/high-contrast-theme.const';
+import { arsnova, arsnova_meta } from './arsnova-theme/arsnova-theme.const';
 
 export const themes = {
-  light: light,
-  dark: dark,
-  beamer: beamer,
-  highContrast: highContrast
+  arsnova: arsnova,
+  dark: dark
 };
 
 export const themes_meta = {
-  light: light_meta,
-  dark: dark_meta,
-  beamer: beamer_meta,
-  highContrast: highContrast_meta
+  arsnova: arsnova_meta,
+  dark: dark_meta
 };
