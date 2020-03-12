9f12d2a6cd66b5730f2e15e194ae66d10ae533fd
Lukas Mauß <lukas.mauss@gmx.de>
2020-03-12 23:38:34 +0100

    Use arsnova-theme as default

--- src/theme/theme.directive.ts
+++ src/theme/theme.directive.ts
@@ -10,7 +10,7 @@ import { Subscription } from 'rxjs';
 
 export class ThemeDirective implements OnInit, OnDestroy {
 
-  private themeName = 'dark';
+  private themeName = 'arsnova';
   private themServiceSubscription: Subscription;
 
   constructor(private elementRef: ElementRef,
