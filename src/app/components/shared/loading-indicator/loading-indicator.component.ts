8ce6bc1c34ed424f30f37be221024218e82ae304
Lukas Mauß <lukas.mauss@gmx.de>
2020-03-21 15:22:51 +0100

    Create component for loading indicator

---
+++ src/app/components/shared/loading-indicator/loading-indicator.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-indicator',
+  templateUrl: './loading-indicator.component.html',
+  styleUrls: ['./loading-indicator.component.scss']
+})
+export class LoadingIndicatorComponent implements OnInit {
+
+  @Input() size = 100;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
