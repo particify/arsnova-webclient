e1700b5093e4aa8c9de41ae866983aad44a9c197
Lukas Mauß <lukas.mauss@gmx.de>
2020-03-25 14:11:03 +0100

    Add dialog component for info dialogs and expand service

--- src/app/services/util/dialog.service.ts
+++ src/app/services/util/dialog.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { YesNoDialogComponent } from '../../components/shared/_dialogs/yes-no-dialog/yes-no-dialog.component';
+import { InfoDialogComponent } from '../../components/shared/_dialogs/info-dialog/info-dialog.component';
 
 @Injectable()
 export class DialogService {
@@ -19,6 +20,16 @@ export class DialogService {
         abortLabel: 'abort',
         type: 'button-warn',
         bodyElement: bodyElement,
+      }
+    });
+  }
+
+  openInfoDialog(section: string, body: string): void {
+    this.dialog.open(InfoDialogComponent, {
+      'width': '80%',
+      data: {
+        section: section,
+        body: body
       }
     });
   }
