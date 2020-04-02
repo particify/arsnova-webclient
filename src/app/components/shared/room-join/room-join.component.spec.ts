e5e6a1b2e522c692d99b5cbfbee2afecae9b02bb
tekay <tom.kaesler@mni.thm.de>
2020-04-03 00:18:55 +0200

    Fix 'created' test for room join

--- src/app/components/shared/room-join/room-join.component.spec.ts
+++ src/app/components/shared/room-join/room-join.component.spec.ts
@@ -1,6 +1,77 @@
-/* import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RoomJoinComponent } from './room-join.component';
+import { Injectable, Component } from '@angular/core';
+import { TranslateService, TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { RoomService } from '../../../services/http/room.service';
+import { Router } from '@angular/router';
+import { NotificationService } from '../../../services/util/notification.service';
+import { AuthenticationService } from '../../../services/http/authentication.service';
+import { ModeratorService } from '../../../services/http/moderator.service';
+import { EventService } from '../../../services/util/event.service';
+import { Observable, of } from 'rxjs';
+
+const TRANSLATION_DE = require('../../../../assets/i18n/home/de.json');
+const TRANSLATION_EN = require('../../../../assets/i18n/home/en.json');
+
+const TRANSLATIONS = {
+  DE: TRANSLATION_DE,
+  EN: TRANSLATION_EN
+};
+
+class JsonTranslationLoader implements TranslateLoader {
+  getTranslation(code: string = ''): Observable<object> {
+    if (code !== null) {
+      const uppercased = code.toUpperCase();
+
+      return of(TRANSLATIONS[uppercased]);
+    } else {
+      return of({});
+    }
+  }
+}
+
+@Injectable()
+class MockRoomService {
+
+}
+
+@Injectable()
+class MockRouter {
+
+}
+
+@Injectable()
+class MockNotificationService {
+
+}
+
+@Injectable()
+class MockAuthenticationService {
+  public watchUser = jasmine.createSpyObj('MockAuthenticationServiceWatchUserSpy', ['subscribe']);
+}
+
+@Injectable()
+class MockModeratorService {
+
+}
+
+@Injectable()
+class MockEventService {
+
+}
+
+@Component({ selector: 'mat-icon', template: '' })
+class MatIconStubComponent { }
+
+@Component({ selector: 'mat-placeholder', template: '' })
+class MatPlaceholderStubComponent { }
+
+@Component({ selector: 'mat-form-field', template: '' })
+class MatFormFieldStubComponent { }
+
+@Component({ selector: 'mat-error', template: '' })
+class MatErrorStubComponent { }
 
 describe('RoomJoinComponent', () => {
   let component: RoomJoinComponent;
@@ -8,19 +79,61 @@ describe('RoomJoinComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RoomJoinComponent ]
+      declarations: [
+        RoomJoinComponent,
+        MatIconStubComponent,
+        MatPlaceholderStubComponent,
+        MatFormFieldStubComponent,
+        MatErrorStubComponent
+      ],
+      providers: [
+        {
+          provide: RoomService,
+          useClass: MockRoomService
+        },
+        {
+          provide: Router,
+          useClass: MockRouter
+        },
+        {
+          provide: NotificationService,
+          useClass: MockNotificationService
+        },
+        {
+          provide: AuthenticationService,
+          useClass: MockAuthenticationService
+        },
+        {
+          provide: ModeratorService,
+          useClass: MockModeratorService
+        },
+        {
+          provide: EventService,
+          useClass: MockEventService
+        },
+      ],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: JsonTranslationLoader
+          },
+          isolate: true
+        })
+      ],
     })
-    .compileComponents();
+    .compileComponents()
+    .then(() => {
+      fixture = TestBed.createComponent(RoomJoinComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(RoomJoinComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
- */
