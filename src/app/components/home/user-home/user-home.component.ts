f21cdb06304d37100ab64e5da8e1d961f0c4a55c
Lukas Mauß <lukas.mauss@gmx.de>
2020-04-02 14:03:25 +0200

    Make user view accessible
    * Adjust keyboard shortcuts
    * Fix tabing
    * Use HostListener instead of Renderer2

--- src/app/components/home/user-home/user-home.component.ts
+++ src/app/components/home/user-home/user-home.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { AfterContentInit, Component, HostListener, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { LanguageService } from '../../../services/util/language.service';
 import { User } from '../../../models/user';
@@ -14,54 +14,50 @@ import { DialogService } from '../../../services/util/dialog.service';
   templateUrl: './user-home.component.html',
   styleUrls: ['./user-home.component.scss']
 })
-export class UserHomeComponent implements OnInit, OnDestroy, AfterContentInit {
+export class UserHomeComponent implements OnInit, AfterContentInit {
   user: User;
 
-  listenerFn: () => void;
-
-  constructor(
+    constructor(
     private dialogService: DialogService,
     private translateService: TranslateService,
     protected langService: LanguageService,
     private authenticationService: AuthenticationService,
     private eventService: EventService,
     private liveAnnouncer: LiveAnnouncer,
-    private _r: Renderer2
   ) {
     langService.langEmitter.subscribe(lang => translateService.use(lang));
   }
 
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Digit1) === true && this.eventService.focusOnInput === false) {
+      document.getElementById('create-session-button').focus();
+    } else if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Digit3) === true && this.eventService.focusOnInput === false) {
+      document.getElementById('session-id-input').focus();
+    } else if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Digit4) === true && this.eventService.focusOnInput === false) {
+      document.getElementById('room-list').focus();
+    } else if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Escape, KeyboardKey.Digit9) === true &&
+      this.eventService.focusOnInput === false) {
+      this.announce();
+    }
+  }
+
   ngAfterContentInit(): void {
     setTimeout(() => {
-      document.getElementById('live_announcer-button').focus();
-    }, 700);
+      document.getElementById('user-message').focus();
+    }, 500);
   }
 
   ngOnInit() {
     this.translateService.use(localStorage.getItem('currentLang'));
     this.authenticationService.watchUser.subscribe(newUser => this.user = newUser);
-    this.listenerFn = this._r.listen(document, 'keyup', (event) => {
-      if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Digit1) === true && this.eventService.focusOnInput === false) {
-        document.getElementById('create_session-button').focus();
-      } else if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Digit3) === true && this.eventService.focusOnInput === false) {
-        document.getElementById('create_session-button').focus();
-      } else if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Escape, KeyboardKey.Digit9) === true &&
-        this.eventService.focusOnInput === false) {
-        this.announce();
-      }
-    });
-  }
-
-  ngOnDestroy() {
-    this.listenerFn();
   }
 
   public announce() {
     this.liveAnnouncer.clear();
-    this.liveAnnouncer.announce('Du befindest dich auf deiner Benutzer-Seite. ' +
-      'Drücke die Taste 1 um eine neue Sitzung zu erstellen, ' +
-      'die Taste 2 um zum Sitzungs-Menü zu gelangen, die Taste 0 um zurück zur Startseite zu gelangen, ' +
-      'oder die Taste 9 um diese Ansage zu wiederholen.', 'assertive');
+    this.translateService.get('home-page.a11y-user-keys').subscribe(msg => {
+      this.liveAnnouncer.announce(msg, 'assertive');
+    });
   }
 
   openCreateRoomDialog(): void {
