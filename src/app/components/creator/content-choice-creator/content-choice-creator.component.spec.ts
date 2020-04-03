8e6dcdd0bb832fb80769e6bd2526c714798e9402
tekay <tom.kaesler@mni.thm.de>
2020-04-03 10:29:06 +0200

    Fix 'create' test for content choice creator component

--- src/app/components/creator/content-choice-creator/content-choice-creator.component.spec.ts
+++ src/app/components/creator/content-choice-creator/content-choice-creator.component.spec.ts
@@ -1,6 +1,95 @@
-/* import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContentChoiceCreatorComponent } from './content-choice-creator.component';
+import { Component, Injectable, Input } from '@angular/core';
+import { ContentService } from '../../../services/http/content.service';
+import { NotificationService } from '../../../services/util/notification.service';
+import { MatDialog } from '@angular/material/dialog';
+import { TranslateService, TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { EventService } from '../../../services/util/event.service';
+import { RoomService } from '../../../services/http/room.service';
+import { Observable, of } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+
+const TRANSLATION_DE = require('../../../../assets/i18n/home/de.json');
+const TRANSLATION_EN = require('../../../../assets/i18n/home/en.json');
+
+const TRANSLATIONS = {
+  DE: TRANSLATION_DE,
+  EN: TRANSLATION_EN
+};
+
+class JsonTranslationLoader implements TranslateLoader {
+  getTranslation(code: string = ''): Observable<object> {
+    if (code !== null) {
+      const uppercased = code.toUpperCase();
+
+      return of(TRANSLATIONS[uppercased]);
+    } else {
+      return of({});
+    }
+  }
+}
+
+@Injectable()
+class MockContentService {
+
+}
+
+@Injectable()
+class MockNotificationService {
+
+}
+
+@Injectable()
+class MockMatDialiog {
+
+}
+
+@Injectable()
+class MockEventService {
+
+}
+
+@Injectable()
+class MockRoomService {
+
+}
+
+@Component({ selector: 'mat-icon', template: '' })
+class MatIconStubComponent { }
+
+@Component({ selector: 'mat-form-field', template: '' })
+class MatFormFieldStubComponent { }
+
+@Component({ selector: 'mat-checkbox', template: '' })
+class MatCheckboxStubComponent { }
+
+@Component({ selector: 'mat-list', template: '' })
+class MatListStubComponent { }
+
+@Component({ selector: 'mat-list-item', template: '' })
+class MatListItemStubComponent { }
+
+@Component({ selector: 'mat-radio-group', template: '' })
+class MatRadioGroupStubComponent {
+  @Input() ngModel;
+  @Input() ngModelOptions;
+}
+
+@Component({ selector: 'mat-radio-button', template: '' })
+class MatRadioButtonStubComponent {
+  @Input() value;
+  @Input() checked;
+}
+
+@Component({ selector: 'mat-divider', template: '' })
+class MatDividerStubComponent { }
+
+@Component({ selector: 'input', template: '' })
+class InputStubComponent {
+  @Input() ngModel;
+}
 
 describe('ContentChoiceCreatorComponent', () => {
   let component: ContentChoiceCreatorComponent;
@@ -8,19 +97,59 @@ describe('ContentChoiceCreatorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContentChoiceCreatorComponent ]
+      declarations: [
+        ContentChoiceCreatorComponent,
+        MatIconStubComponent,
+        MatFormFieldStubComponent,
+        MatCheckboxStubComponent,
+        MatListStubComponent,
+        MatListItemStubComponent,
+        MatRadioGroupStubComponent,
+        MatRadioButtonStubComponent,
+        MatDividerStubComponent,
+        InputStubComponent
+      ],
+      providers: [
+        {
+          provide: ContentService,
+          useClass: MockContentService
+        },
+        {
+          provide: NotificationService,
+          useClass: MockNotificationService
+        },
+        {
+          provide: MatDialog,
+          useClass: MockMatDialiog
+        },
+        {
+          provide: EventService,
+          useClass: MockEventService
+        },
+        {
+          provide: RoomService,
+          useClass: MockRoomService
+        },
+      ],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: JsonTranslationLoader
+          },
+          isolate: true
+        })
+      ],
     })
-    .compileComponents();
+    .compileComponents()
+    .then(() => {
+      fixture = TestBed.createComponent(ContentChoiceCreatorComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
   }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ContentChoiceCreatorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
- */
