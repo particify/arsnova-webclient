5d08abcb9c4d7b239d199d85566f2c347199cf54
Lukas Mauß <lukas.mauss@gmx.de>
2020-04-04 20:21:14 +0200

    Make general content-create-view accessible

--- src/app/components/creator/content-create-page/content-create-page.component.ts
+++ src/app/components/creator/content-create-page/content-create-page.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { LanguageService } from '../../../services/util/language.service';
 import { ContentGroup } from '../../../models/content-group';
 import { ContentText } from '../../../models/content-text';
 import { FormControl } from '@angular/forms';
 import { EventService } from '../../../services/util/event.service';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { KeyboardUtils } from '../../../utils/keyboard';
+import { KeyboardKey } from '../../../utils/keyboard/keys';
+import { MatTabGroup } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-content-create-page',
   templateUrl: './content-create-page.component.html',
   styleUrls: ['./content-create-page.component.scss']
 })
-export class ContentCreatePageComponent implements OnInit {
+export class ContentCreatePageComponent implements OnInit, AfterContentInit {
 
   contentGroups: string[] = [];
   lastCollection: ContentGroup;
@@ -30,7 +34,8 @@ export class ContentCreatePageComponent implements OnInit {
 
   constructor(private translateService: TranslateService,
               protected langService: LanguageService,
-              public eventService: EventService) {
+              public eventService: EventService,
+              private liveAnnouncer: LiveAnnouncer) {
     langService.langEmitter.subscribe(lang => translateService.use(lang));
   }
 
@@ -52,6 +57,25 @@ export class ContentCreatePageComponent implements OnInit {
     }
   }
 
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    const focusOnInput = this.eventService.focusOnInput;
+    if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Digit1) === true && focusOnInput === false) {
+      document.getElementById('subject-input').focus();
+    } else if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Digit3) === true && focusOnInput === false) {
+      document.getElementById('format-info-button').focus();
+    } else if (KeyboardUtils.isKeyEvent(event, KeyboardKey.Escape) === true) {
+      document.getElementById('keys-announcer-button').focus();
+    }
+  }
+
+  ngAfterContentInit() {
+    setTimeout(() => {
+      document.getElementById('message-announcer-button').focus();
+    }, 700);
+    this.eventService.makeFocusOnInputFalse();
+  }
+
   ngOnInit() {
     this.translateService.use(localStorage.getItem('currentLang'));
     const lastGroup: ContentGroup = JSON.parse(sessionStorage.getItem('lastGroup'));
@@ -59,7 +83,13 @@ export class ContentCreatePageComponent implements OnInit {
       this.lastCollection = lastGroup ? lastGroup.name : defaultGroup;
     });
     this.getGroups();
+  }
 
+  announce() {
+    this.liveAnnouncer.clear();
+    this.translateService.get('content.a11y-content-create-keys').subscribe(msg => {
+      this.liveAnnouncer.announce(msg, 'assertive');
+    });
   }
 
   getGroups(): void {
