7aa795576440c9080ac81b7019bc5acbde2522ca
Lukas Mauß <lukas.mauss@gmx.de>
2020-04-11 10:43:20 +0200

    Subscribe to feedback stream on participants room page

--- src/app/components/participant/room-participant-page/room-participant-page.component.ts
+++ src/app/components/participant/room-participant-page/room-participant-page.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Component, HostListener, OnInit } from '@angular/core';
+import { AfterContentInit, Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { Room } from '../../../models/room';
 import { User } from '../../../models/user';
 import { UserRole } from '../../../models/user-roles.enum';
@@ -17,16 +17,21 @@ import { EventService } from '../../../services/util/event.service';
 import { KeyboardUtils } from '../../../utils/keyboard';
 import { KeyboardKey } from '../../../utils/keyboard/keys';
 import { NotificationService } from '../../../services/util/notification.service';
+import { Message } from '@stomp/stompjs';
+import { Subscription } from 'rxjs';
+import { WsFeedbackService } from '../../../services/websockets/ws-feedback.service';
 
 @Component({
   selector: 'app-room-participant-page',
   templateUrl: './room-participant-page.component.html',
   styleUrls: ['./room-participant-page.component.scss']
 })
-export class RoomParticipantPageComponent extends RoomPageComponent implements OnInit, AfterContentInit {
+export class RoomParticipantPageComponent extends RoomPageComponent implements OnInit, AfterContentInit, OnDestroy {
 
   room: Room;
   user: User;
+  protected feedbackSub: Subscription;
+  feedbackEnabled = false;
 
   constructor(protected location: Location,
               protected roomService: RoomService,
@@ -40,7 +45,8 @@ export class RoomParticipantPageComponent extends RoomPageComponent implements O
               protected contentService: ContentService,
               protected authenticationService: AuthenticationService,
               private liveAnnouncer: LiveAnnouncer,
-              public eventService: EventService) {
+              public eventService: EventService,
+              private wsFeedbackService: WsFeedbackService) {
     super(roomService, route, router, location, wsCommentService, commentService, eventService, contentService, translateService,
       notificationService);
     langService.langEmitter.subscribe(lang => translateService.use(lang));
@@ -82,6 +88,15 @@ export class RoomParticipantPageComponent extends RoomPageComponent implements O
     this.translateService.use(localStorage.getItem('currentLang'));
   }
 
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
+    if (this.feedbackSub) {
+      this.feedbackSub.unsubscribe();
+    }
+  }
+
   public announce() {
     this.liveAnnouncer.clear();
     this.translateService.get('room-page.a11y-keys').subscribe(msg => {
@@ -100,9 +115,22 @@ export class RoomParticipantPageComponent extends RoomPageComponent implements O
     }
     this.authenticationService.setAccess(this.room.shortId, UserRole.PARTICIPANT);
     this.authenticationService.checkAccess(this.room.shortId);
+    this.feedbackEnabled = !this.room.settings['feedbackLocked'];
+    this.feedbackSub = this.wsFeedbackService.getFeedbackStream(this.room.id).subscribe((message: Message) => {
+      this.parseFeedbackMessage(message);
+    });
   }
 
   protected afterGroupsLoadHook() {
     this.isLoading = false;
+  }
+
+  parseFeedbackMessage(message: Message) {
+    const msg = JSON.parse(message.body);
+    if (msg.type === 'FeedbackStarted') {
+      this.feedbackEnabled = true;
+    } else if (msg.type === 'FeedbackStopped') {
+      this.feedbackEnabled = false;
+    }
   }
 }
