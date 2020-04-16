6084324db408096ce6aa18dad8530766ffa3e09f
Lukas Mauß <lukas.mauss@gmx.de>
2020-04-16 14:02:14 +0200

    Fix tests

--- src/app/components/home/import/import.component.spec.ts
+++ src/app/components/home/import/import.component.spec.ts
@@ -1,7 +1,54 @@
-/*
+import { Injectable } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { Observable, of } from 'rxjs';
+import { AuthenticationService } from '../../../services/http/authentication.service';
 import { ImportComponent } from './import.component';
+import { RoomService } from '../../../services/http/room.service';
+import { NotificationService } from '../../../services/util/notification.service';
+import { Router } from '@angular/router';
+
+const TRANSLATION_DE = require('../../../../assets/i18n/home/de.json');
+const TRANSLATION_EN = require('../../../../assets/i18n/home/en.json');
+
+const TRANSLATIONS = {
+  DE: TRANSLATION_DE,
+  EN: TRANSLATION_EN
+};
+
+class JsonTranslationLoader implements TranslateLoader {
+  getTranslation(code: string = ''): Observable<object> {
+    if (code !== null) {
+      const uppercased = code.toUpperCase();
+
+      return of(TRANSLATIONS[uppercased]);
+    } else {
+      return of({});
+    }
+  }
+}
+
+@Injectable()
+class MockRouter {
+
+}
+
+@Injectable()
+class MockRoomService {
+
+}
+
+@Injectable()
+class MockAuthenticationService {
+  isLoggedIn() {
+    return true;
+  }
+}
+
+@Injectable()
+class MockNotificationService {
+
+}
 
 describe('ImportComponent', () => {
   let component: ImportComponent;
@@ -9,19 +56,45 @@ describe('ImportComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ImportComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        ImportComponent,
+      ],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: JsonTranslationLoader
+          },
+          isolate: true
+        })
+      ],
+      providers: [
+        {
+          provide: Router,
+          useClass: MockRouter
+        },
+        {
+          provide: NotificationService,
+          useClass: MockNotificationService
+        },
+        {
+          provide: RoomService,
+          useClass: MockRoomService
+        },
+        {
+          provide: AuthenticationService,
+          useClass: MockAuthenticationService
+        }
+      ]
+    }).compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(ImportComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+      });
   }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ImportComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
-*/
