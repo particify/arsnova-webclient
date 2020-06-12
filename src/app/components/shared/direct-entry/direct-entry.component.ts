cbd04a44eb9c34056c983bb2a50234c0a05c153b
Lukas Mauß <lukas.mauss@gmx.de>
2020-06-12 12:55:59 +0200

    Check if there exists a room with entered id on direct entry

--- src/app/components/shared/direct-entry/direct-entry.component.ts
+++ src/app/components/shared/direct-entry/direct-entry.component.ts
@@ -1,18 +1,34 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { RoomService } from '../../../services/http/room.service';
+import { TranslateService } from '@ngx-translate/core';
+import { NotificationService } from '../../../services/util/notification.service';
 
 @Component({
   templateUrl: './direct-entry.component.html'
 })
 export class DirectEntryComponent {
+
   constructor(
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private roomService: RoomService,
+    private translateService: TranslateService,
+    private notificationService: NotificationService
   ) {
     this.route.params.subscribe(params => {
       const shortId = params['shortId'];
-      // ToDo: Maybe check if valid short id
-      this.router.navigate(['/participant/room/' + shortId]);
+      this.roomService.getRoomByShortId(shortId).subscribe(room => {
+        if (room) {
+          this.router.navigate(['/participant/room/' + shortId]);
+        }
+        },
+        error => {
+          this.translateService.get('errors.room-not-found').subscribe(msg => {
+            this.notificationService.show(msg);
+          });
+          this.router.navigate(['/home']);
+        });
     });
   }
 }
