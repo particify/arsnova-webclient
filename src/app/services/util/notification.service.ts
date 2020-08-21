8a73e3a7065776392244be4ffced9dd686b23cc7
Lukas Mauß <lukas.mauss@gmx.de>
2020-08-21 17:23:09 +0200

    Implement advanced snack bar notifications
    Use icons as follows:
    * 'SUCCESS' for success actions like content created, logged in etc.
    * 'WARNING' for invalid inputs and important informations
    * 'FAILED' for unsuccessful actions like incorrect login data, duplicated content group etc.

--- src/app/services/util/notification.service.ts
+++ src/app/services/util/notification.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+import { SnackBarAdvancedComponent } from '../../components/shared/snack-bar-advanced/snack-bar-advanced.component';
+
+export enum AdvancedSnackBarTypes {
+  SUCCESS = 'SUCCESS',
+  FAILED = 'FAILED',
+  WARNING = 'WARNING'
+}
 
 @Injectable()
 export class NotificationService {
@@ -16,5 +23,26 @@ export class NotificationService {
 
     // Delegate the message and merge the (optionally) passed config with the default config
     this.snackRef = this.snackBar.open(message, action, Object.assign({}, defaultConfig, config));
+  }
+
+  showAdvanced(message: string, type: string) {
+    let typeString: string;
+    switch (type) {
+      case AdvancedSnackBarTypes.SUCCESS:
+        typeString = 'done';
+        break;
+      case AdvancedSnackBarTypes.FAILED:
+        typeString = 'close';
+        break;
+      case AdvancedSnackBarTypes.WARNING:
+        typeString = 'warning';
+    }
+    this.snackRef = this.snackBar.openFromComponent(SnackBarAdvancedComponent, {
+      data: {
+        message: message,
+        icon: typeString
+      },
+      duration: type === AdvancedSnackBarTypes.FAILED ? 4000 : 2500
+    });
   }
 }
