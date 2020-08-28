c20f841df47265d867f7b5b0731cb8a6d880e997
Daniel Gerhardt <code@dgerhardt.net>
2020-08-28 22:15:39 +0200

    Refresh answer statistics on change events
    A WebSocket STOMP subscription is used to receive events regarding
    answer changes. After an event was received the statistics are reloaded
    from the backend and the chart is updated.
    
    Depends-On: arsnova-backend!278

--- src/app/components/shared/statistic-choice/statistic-choice.component.ts
+++ src/app/components/shared/statistic-choice/statistic-choice.component.ts
@@ -9,7 +9,7 @@ import { Content } from '../../../models/content';
 import { ContentType } from '../../../models/content-type.enum';
 import { AnswerStatistics } from '../../../models/answer-statistics';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { startWith, takeUntil } from 'rxjs/operators';
 
 export class AnswerList {
   label: string;
@@ -62,12 +62,18 @@ export class StatisticChoiceComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.chartId = 'chart-' + this.content.id;
     this.checkIfSurvey(this.content);
-    this.loadData().subscribe(stats => {
-      this.updateData(stats);
-      if (this.directShow) {
-        this.toggleChart(true);
-      }
-      this.isLoading = false;
+    this.contentService.getAnswersChangedStream(this.content.roomId, this.content.id).pipe(
+        /* startWith: Execute once even when no event has been received */
+        startWith(null),
+        takeUntil(this.destroyed$)
+    ).subscribe(() => {
+      this.loadData().subscribe(stats => {
+        this.updateData(stats);
+        if (this.directShow) {
+          this.toggleChart(true);
+        }
+        this.isLoading = false;
+      });
     });
   }
 
