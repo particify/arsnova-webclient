8b17faae5c1335763df6be41961416df62dc3ce3
Lukas Mauß <lukas.mauss@gmx.de>
2020-09-01 20:09:45 +0200

    Move most of footer items to 'user-menu' and optimize items
    * Add footer items to user menu and use submenu if needed
    * Change 'introduction' to 'help' which navigates to external page
    * Keep showing legal information in footer, but smaller
    * Remove 'home'-option in user menu and use home-icon for 'my-rooms'

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -6,10 +6,9 @@ import { TranslateService } from '@ngx-translate/core';
 import { AuthenticationService } from '../../../services/http/authentication.service';
 import { Room } from '../../../models/room';
 import { ThemeService } from '../../../../theme/theme.service';
-import { Theme } from '../../../../theme/Theme';
 import { DialogService } from '../../../services/util/dialog.service';
 import { ApiConfigService } from '../../../services/http/api-config.service';
-import { GlobalStorageService, STORAGE_KEYS } from '../../../services/util/global-storage.service';
+import { GlobalStorageService } from '../../../services/util/global-storage.service';
 import { ConsentService } from '../../../services/util/consent.service';
 
 @Component({
@@ -21,16 +20,8 @@ export class FooterComponent implements OnInit {
 
   public room: Room;
 
-  public open: string;
-  public deviceWidth = innerWidth;
-  public lang: string;
-
-  public themeClass: String;
-
-  public themes: Theme[];
   privacyUrl: string;
   imprintUrl: string;
-  manualUrl: string;
 
   constructor(
     public notificationService: NotificationService,
@@ -44,38 +35,22 @@ export class FooterComponent implements OnInit {
     private globalStorageService: GlobalStorageService,
     private consentService: ConsentService
   ) {
-    this.themeClass = this.globalStorageService.getItem(STORAGE_KEYS.THEME);
-    this.lang = this.globalStorageService.getItem(STORAGE_KEYS.LANGUAGE);
     langService.langEmitter.subscribe(lang => translateService.use(lang));
   }
 
   ngOnInit() {
-    this.themeService.getTheme().subscribe(theme => {
-      this.themeClass = theme;
-    });
-    this.translateService.use(this.lang);
-    this.translateService.get('footer.open').subscribe(message => {
-      this.open = message;
-    });
-    this.themes = this.themeService.getThemes();
-
     if (this.consentService.consentRequired()) {
       this.consentService.openDialog();
     }
 
     this.apiConfigService.getApiConfig$().subscribe(config => {
-      this.manualUrl = config.ui.links.manual.url;
       this.privacyUrl = config.ui.links.privacy.url;
-      this.imprintUrl = config.ui.links.tos.imprint;
+      this.imprintUrl = config.ui.links.imrpint.url ;
     });
   }
 
   openUrlInNewTab(url: string) {
     window.open(url, '_blank');
-  }
-
-  showManual() {
-   this.openUrlInNewTab(this.manualUrl);
   }
 
   showImprint() {
@@ -88,20 +63,5 @@ export class FooterComponent implements OnInit {
 
   showCookieSettings() {
     this.consentService.openDialog();
-  }
-
-  useLanguage(language: string) {
-    this.translateService.use(language);
-    this.globalStorageService.setItem(STORAGE_KEYS.LANGUAGE, language);
-    this.langService.langEmitter.emit(language);
-  }
-
-  changeTheme(theme: Theme) {
-    this.themeClass = theme.key;
-    this.themeService.activate(theme.key);
-  }
-
-  getLanguage(): string {
-    return this.globalStorageService.getItem(STORAGE_KEYS.LANGUAGE);
   }
 }
