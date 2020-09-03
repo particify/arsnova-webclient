e7d2cabaac69163757a55922371e077954570864
Lukas Mauß <lukas.mauss@gmx.de>
2020-09-03 11:56:28 +0200

    Refactor statistic help component and remove irrelevant status

--- src/app/components/shared/_dialogs/statistic-help/statistic-help.component.ts
+++ src/app/components/shared/_dialogs/statistic-help/statistic-help.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
 
+class GroupStatus {
+  name: string;
+  value: string;
+
+  constructor(name: string, value: string) {
+    this.name = name;
+    this.value = value;
+  }
+}
+
 @Component({
   selector: 'app-statistic-help',
   templateUrl: './statistic-help.component.html',
   styleUrls: ['./statistic-help.component.scss']
 })
+
 export class StatisticHelpComponent {
+
+  chips: GroupStatus[] = [
+    new GroupStatus('good', ' > 85 % '),
+    new GroupStatus('okay', ' >= 50 % '),
+    new GroupStatus('negative',  '< 50 %'),
+  ];
 
   constructor() {
   }
