5e90f6efdfcad9a04e036a61490295e46709893d
Daniel Gerhardt <code@dgerhardt.net>
2020-11-11 10:20:08 +0100

    Adjust paths to support non-root base hrefs
    The app can now be deployed to subfolders.

--- src/app/translate-http-loader-factory.ts
+++ src/app/translate-http-loader-factory.ts
@@ -2,5 +2,5 @@ import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export function TranslateHttpLoaderFactory(http: HttpClient, moduleName: string) {
-  return new TranslateHttpLoader(http, `../../assets/i18n/${moduleName}/`, '.json');
+  return new TranslateHttpLoader(http, `assets/i18n/${moduleName}/`, '.json');
 }
