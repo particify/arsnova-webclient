82feb4f02bb86b6bf7b4c6130902de293e30d839
Daniel Gerhardt <code@dgerhardt.net>
2021-04-02 21:28:01 +0200

    Show a snackbar if config is not loaded immediately
    A snackbar with loading spinner animation is shown if the config is not
    loaded within one second.
    
    Closes: #706

--- src/app/resolver/api-config.resolver.ts
+++ src/app/resolver/api-config.resolver.ts
@@ -1,17 +1,35 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBarRef } from '@angular/material/snack-bar';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
+import { Observable, timer } from 'rxjs';
+import { takeUntil, tap } from 'rxjs/operators';
+import { SnackBarAdvancedComponent } from '../components/shared/snack-bar-advanced/snack-bar-advanced.component';
 import { ApiConfig } from '../models/api-config';
 import { ApiConfigService } from '../services/http/api-config.service';
+import { AdvancedSnackBarTypes, NotificationService } from '../services/util/notification.service';
 
 @Injectable()
 export class ApiConfigResolver implements Resolve<ApiConfig> {
   constructor(
-    private apiConfigService: ApiConfigService
+    private apiConfigService: ApiConfigService,
+    private notificationService: NotificationService,
+    private translateService: TranslateService
   ) {
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<ApiConfig> {
-    return this.apiConfigService.getApiConfig$();
+    let snackbarRef: MatSnackBarRef<SnackBarAdvancedComponent>;
+    const config$ = this.apiConfigService.getApiConfig$().pipe(
+      tap(() => snackbarRef?.dismiss()));
+    // Show a snackbar if the config is not loaded within one second.
+    timer(1000).pipe(
+      takeUntil(config$),
+      tap(() => {
+        this.translateService.get('errors.establishing-connection').subscribe(msg =>
+          snackbarRef = this.notificationService.showAdvanced(msg, AdvancedSnackBarTypes.LOADING));
+      }))
+      .subscribe();
+    return config$;
   }
 }
