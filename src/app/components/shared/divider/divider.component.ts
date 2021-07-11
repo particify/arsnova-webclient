710be7b0aee9bc11c0668aa9e4682a33904f92e3
Lukas Mauß <lukas.mauss@gmx.de>
2021-07-11 20:57:08 +0200

    Redesign room overview
    - Reduce action icon buttons to 'comments' and 'live-survey'
    - Use divider to seperate feature sections and extend divider component for optinal padding
    - Make room views for all roles consistent and share styling

--- src/app/components/shared/divider/divider.component.ts
+++ src/app/components/shared/divider/divider.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-divider',
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./divider.component.scss']
 })
 export class DividerComponent {
+
+  @Input() padding = false;
 
   constructor() { }
 
