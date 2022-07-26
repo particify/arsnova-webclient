5e4d54e9ddeed58617cd73725aed16fa84bec253
Daniel Gerhardt <code@dgerhardt.net>
2022-07-26 16:46:32 +0200

    Revert "Add user profile component"
    This reverts commit b6bb99569c37b5860c575d3869f40708c1347607.

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,17 +1,11 @@
-import { Person } from './person';
-
 export class User {
   id: string;
   loginId: string;
   authProvider: string;
-  revision: string;
-  person: Person;
 
-  constructor(id: string, loginId: string, authProvider: string, revision: string, person: Person) {
+  constructor(id: string, loginId: string, authProvider: string) {
     this.id = id;
     this.loginId = loginId;
     this.authProvider = authProvider;
-    this.revision = revision;
-    this.person = person;
   }
 }
