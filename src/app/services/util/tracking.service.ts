c980ae472acadec3f2ab57892bff744b8868491d
Lukas Mauß <lukas.mauss@gmx.de>
2022-09-05 11:29:10 +0200

    Prevent adding parent routes in tracking service
    Otherwise routes will be tracked multiple times because both parent and child routes are added.

--- src/app/services/util/tracking.service.ts
+++ src/app/services/util/tracking.service.ts
@@ -102,8 +102,11 @@ export class TrackingService {
     });
     this.router.events.pipe(
         filter(event => (event instanceof ActivationEnd && !!event.snapshot.component)),
-        filter(event => (event as ActivationEnd).snapshot.outlet === 'primary'))
-        .subscribe((event: ActivationEnd) => this.addRoute(this.router.url, event.snapshot));
+        filter(event => (event as ActivationEnd).snapshot.outlet === 'primary'),
+        filter(event => !(event as ActivationEnd).snapshot.routeConfig.children))
+        .subscribe((event: ActivationEnd) => {
+          this.addRoute(this.router.url, event.snapshot)
+        });
     this.translateService.onLangChange
         .subscribe((event: LangChangeEvent) => this.setVisitDimension(VisitDimension.UI_LANGUAGE, event.lang));
     this.themeService.getTheme().subscribe((themeName) => this.setVisitDimension(VisitDimension.THEME, themeName));
