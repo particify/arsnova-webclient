16028c3981a1f9009fa7afabab5bb433a966f464
Lukas Mauß <lukas.mauss@gmx.de>
2023-01-30 09:55:02 +0100

    Fix styling for content publish dialog

--- src/app/components/creator/_dialogs/publish-content/publish-content.component.ts
+++ src/app/components/creator/_dialogs/publish-content/publish-content.component.ts
@@ -4,7 +4,6 @@ import { MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'app-publish-content',
   templateUrl: './publish-content.component.html',
-  styleUrls: ['./publish-content.component.scss'],
 })
 export class PublishContentComponent {
   readonly dialogId = 'publish-content';
