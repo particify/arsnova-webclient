b3de28b684430602e41d98722f8adfe50471df58
Lukas Mauß <lukas.mauss@gmx.de>
2023-02-07 12:13:26 +0100

    Use cache in comment settings service
    Store comment settings in cache using `AbstractCachingHttpService` and update on websocket events.

--- src/app/services/http/comment-settings.service.ts
+++ src/app/services/http/comment-settings.service.ts
@@ -3,37 +3,52 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CommentSettings } from '../../models/comment-settings';
 import { catchError } from 'rxjs/operators';
-import { AbstractHttpService } from './abstract-http.service';
 import { TranslateService } from '@ngx-translate/core';
 import { NotificationService } from '../util/notification.service';
 import { EventService } from '../util/event.service';
+import { AbstractCachingHttpService } from './abstract-caching-http.service';
+import { CachingService } from '../util/caching.service';
+import { WsConnectorService } from '../websockets/ws-connector.service';
+import { WsCommentService } from '../websockets/ws-comment.service';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
 };
 
 @Injectable()
-export class CommentSettingsService extends AbstractHttpService<CommentSettings> {
+export class CommentSettingsService extends AbstractCachingHttpService<CommentSettings> {
   constructor(
     private http: HttpClient,
+    protected wsConnectorService: WsConnectorService,
     protected eventService: EventService,
     protected translateService: TranslateService,
-    protected notificationService: NotificationService
+    protected notificationService: NotificationService,
+    protected cachingService: CachingService,
+    protected wsCommentService: WsCommentService
   ) {
     super(
       '/settings',
       http,
+      wsConnectorService,
       eventService,
       translateService,
-      notificationService
+      notificationService,
+      cachingService
     );
   }
 
   get(id: string): Observable<CommentSettings> {
     const connectionUrl = this.buildUri(`/${id}`, id);
-    return this.http
-      .get<CommentSettings>(connectionUrl, httpOptions)
-      .pipe(catchError(this.handleError<CommentSettings>('addComment')));
+    if (!this.stompSubscription) {
+      this.stompSubscription = this.wsCommentService
+        .getCommentSettingsStream(id)
+        .subscribe((msg) => {
+          this.handleObjectCaching(connectionUrl, JSON.parse(msg.body).payload);
+        });
+    }
+    return this.fetch(connectionUrl).pipe(
+      catchError(this.handleError<CommentSettings>('addComment'))
+    );
   }
 
   add(settings: CommentSettings): Observable<CommentSettings> {
