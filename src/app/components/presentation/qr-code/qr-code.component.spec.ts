bcd101a8f62ba7d451442a4b2ddb65ab76ba189b
Lukas Mauß <lukas.mauss@gmx.de>
2023-03-31 17:50:22 +0200

    Add tests for `QRCodeComponent`

--- src/app/components/presentation/qr-code/qr-code.component.spec.ts
+++ src/app/components/presentation/qr-code/qr-code.component.spec.ts
@@ -13,14 +13,6 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
 import { RoutingService } from '@arsnova/app/services/util/routing.service';
 
-class MockApiConfigService {
-  getApiConfig$() {
-    return of({ ui: {} });
-  }
-}
-
-class MockRoutingService {}
-
 describe('QrCodeComponent', () => {
   let component: QrCodeComponent;
   let fixture: ComponentFixture<QrCodeComponent>;
@@ -39,6 +31,44 @@ describe('QrCodeComponent', () => {
 
   const activatedRouteStub = new ActivatedRouteStub(null, null, snapshot);
 
+  const mockApiConfigService = jasmine.createSpyObj(ApiConfigService, [
+    'getApiConfig$',
+  ]);
+
+  const configWithJoinLink = {
+    ui: {
+      links: {
+        join: 'https://partici.fi/',
+      },
+    },
+  };
+
+  const configWithoutJoinLink = {
+    ui: {},
+  };
+
+  const mockRoutingService = jasmine.createSpyObj(RoutingService, [
+    'getRoomJoinUrl',
+    'removeProtocolFromUrl',
+  ]);
+  mockRoutingService.getRoomJoinUrl
+    .withArgs('https://partici.fi/')
+    .and.returnValue(
+      ('https://partici.fi/' || document.baseURI + 'p/') + '12345678'
+    );
+  mockRoutingService.getRoomJoinUrl
+    .withArgs(undefined)
+    .and.returnValue((undefined || document.baseURI + 'p/') + '12345678');
+  mockRoutingService.removeProtocolFromUrl
+    .withArgs('http://localhost:9876/')
+    .and.returnValue('localhost:9876/');
+  mockRoutingService.removeProtocolFromUrl
+    .withArgs('http://localhost:9876')
+    .and.returnValue('localhost:9876');
+  mockRoutingService.removeProtocolFromUrl
+    .withArgs('https://partici.fi/12345678')
+    .and.returnValue('partici.fi/12345678');
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [QrCodeComponent, SplitShortIdPipe],
@@ -49,7 +79,7 @@ describe('QrCodeComponent', () => {
         },
         {
           provide: ApiConfigService,
-          useClass: MockApiConfigService,
+          useValue: mockApiConfigService,
         },
         {
           provide: ThemeService,
@@ -61,7 +91,7 @@ describe('QrCodeComponent', () => {
         },
         {
           provide: RoutingService,
-          useClass: MockRoutingService,
+          useValue: mockRoutingService,
         },
       ],
       schemas: [NO_ERRORS_SCHEMA],
@@ -69,12 +99,55 @@ describe('QrCodeComponent', () => {
   }));
 
   beforeEach(() => {
+    mockApiConfigService.getApiConfig$.and.returnValue(of(configWithJoinLink));
     fixture = TestBed.createComponent(QrCodeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('should use correct url if config for join link is set', () => {
+    mockApiConfigService.getApiConfig$.and.returnValue(of(configWithJoinLink));
+    fixture.detectChanges();
+    expect(component.url).toBe('https://partici.fi/12345678');
+  });
+
+  it('should use correct display url if config for join link is set', () => {
+    mockApiConfigService.getApiConfig$.and.returnValue(of(configWithJoinLink));
+    fixture.detectChanges();
+    expect(component.displayUrl).toBe('partici.fi/12345678');
+  });
+
+  it('should use correct qr url if config for join link is set', () => {
+    mockApiConfigService.getApiConfig$.and.returnValue(of(configWithJoinLink));
+    fixture.detectChanges();
+    expect(component.qrUrl).toBe('https://partici.fi/12345678?entry=qr');
+  });
+
+  it('should use correct url if config for join link is NOT set', () => {
+    mockApiConfigService.getApiConfig$.and.returnValue(
+      of(configWithoutJoinLink)
+    );
+    fixture.detectChanges();
+    expect(component.url).toBe('http://localhost:9876/p/12345678');
+  });
+
+  it('should use correct qr url if config for join link is NOT set', () => {
+    mockApiConfigService.getApiConfig$.and.returnValue(
+      of(configWithoutJoinLink)
+    );
+    fixture.detectChanges();
+    expect(component.qrUrl).toBe('http://localhost:9876/p/12345678?entry=qr');
+  });
+
+  it('should use correct display url if config for join link is NOT set', () => {
+    mockApiConfigService.getApiConfig$.and.returnValue(
+      of(configWithoutJoinLink)
+    );
+    fixture.detectChanges();
+    expect(component.displayUrl).toBe('localhost:9876');
   });
 });
