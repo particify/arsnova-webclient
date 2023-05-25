8a14878c75d43cc51e323d0c2e6576b060fce46c
Lukas Mauß <lukas.mauss@gmx.de>
2023-05-25 08:51:53 +0200

    Add tests for language service

--- src/app/core/services/util/language.service.spec.ts
+++ src/app/core/services/util/language.service.spec.ts
@@ -1,34 +1,112 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { LanguageService } from '@app/core/services/util/language.service';
 import { TranslateService } from '@ngx-translate/core';
 import {
-  MockGlobalStorageService,
-  MockTranslateService,
-} from '@testing/test-helpers';
-import { GlobalStorageService } from '@app/core/services/util/global-storage.service';
+  GlobalStorageService,
+  STORAGE_KEYS,
+} from '@app/core/services/util/global-storage.service';
 
 describe('LanguageService', () => {
+  let langService: LanguageService;
+
+  const translateService = jasmine.createSpyObj('TranslateService', [
+    'setDefaultLang',
+    'getBrowserLang',
+    'use',
+  ]);
+
+  const globalStorageService = jasmine.createSpyObj('GlobalStorageService', [
+    'getItem',
+    'setItem',
+  ]);
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         LanguageService,
         {
           provide: TranslateService,
-          useClass: MockTranslateService,
+          useValue: translateService,
         },
         {
           provide: GlobalStorageService,
-          useClass: MockGlobalStorageService,
+          useValue: globalStorageService,
         },
       ],
     });
+    langService = TestBed.inject(LanguageService);
   });
 
-  it('should be created', inject(
-    [LanguageService],
-    (service: LanguageService) => {
-      expect(service).toBeTruthy();
-    }
-  ));
+  it('should be created', () => {
+    expect(langService).toBeTruthy();
+  });
+
+  it('should use english as fallback if language stored in global storage is not supported and browser lang too', () => {
+    const setLangSpy = spyOn(langService, 'setLang');
+    globalStorageService.getItem
+      .withArgs(STORAGE_KEYS.LANGUAGE)
+      .and.returnValue('unsupported');
+    translateService.getBrowserLang.and.returnValue('unsupported');
+    langService.init();
+    expect(setLangSpy).toHaveBeenCalledWith('en');
+  });
+
+  it('should use english as fallback if no language stored in global storage and browser lang is not supported', () => {
+    const setLangSpy = spyOn(langService, 'setLang');
+    globalStorageService.getItem
+      .withArgs(STORAGE_KEYS.LANGUAGE)
+      .and.returnValue(null);
+    translateService.getBrowserLang.and.returnValue('unsupported');
+    langService.init();
+    expect(setLangSpy).toHaveBeenCalledWith('en');
+  });
+
+  it('should use english as fallback if no language stored in global storage and browser lang is supported but not officially', () => {
+    const setLangSpy = spyOn(langService, 'setLang');
+    globalStorageService.getItem
+      .withArgs(STORAGE_KEYS.LANGUAGE)
+      .and.returnValue(null);
+    translateService.getBrowserLang.and.returnValue('es');
+    langService.init();
+    expect(setLangSpy).toHaveBeenCalledWith('en');
+  });
+
+  it('should use language stored in global storage if supported', () => {
+    const setLangSpy = spyOn(langService, 'setLang');
+    globalStorageService.getItem
+      .withArgs(STORAGE_KEYS.LANGUAGE)
+      .and.returnValue('de');
+    langService.init();
+    expect(setLangSpy).toHaveBeenCalledWith('de');
+  });
+
+  it('should use language stored in global storage if supported even if not officially', () => {
+    const setLangSpy = spyOn(langService, 'setLang');
+    globalStorageService.getItem
+      .withArgs(STORAGE_KEYS.LANGUAGE)
+      .and.returnValue('es');
+    langService.init();
+    expect(setLangSpy).toHaveBeenCalledWith('es');
+  });
+
+  it('should use browser lang if no language stored in global storage and browser lang is supported', () => {
+    const setLangSpy = spyOn(langService, 'setLang');
+    globalStorageService.getItem
+      .withArgs(STORAGE_KEYS.LANGUAGE)
+      .and.returnValue(null);
+    translateService.getBrowserLang.and.returnValue('de');
+    langService.init();
+    expect(setLangSpy).toHaveBeenCalledWith('de');
+  });
+
+  it('should use browser lang if language stored in global storage is not supported but browser lang', () => {
+    const setLangSpy = spyOn(langService, 'setLang');
+    globalStorageService.getItem
+      .withArgs(STORAGE_KEYS.LANGUAGE)
+      .and.returnValue('unsupported');
+    translateService.getBrowserLang.and.returnValue('de');
+    langService.init();
+    expect(setLangSpy).toHaveBeenCalledWith('de');
+  });
 });
