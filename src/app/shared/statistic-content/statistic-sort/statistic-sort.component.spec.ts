9bb8cbc0cb2ddf49b0e5445a04b9a67fd7ad5035
Lukas Mauß <lukas.mauss@gmx.de>
2023-07-21 17:15:15 +0200

    Show correct combination in sort statistic chart no matter how often it was selected
    To be able to get the answer count or percentage of participants answered with the correct sort, it is necessary to display the combination in chart no matter if other cominations have been selected more often.
    
    The correct option is then moved to the position before the sum of other combinations.

--- src/app/shared/statistic-content/statistic-sort/statistic-sort.component.spec.ts
+++ src/app/shared/statistic-content/statistic-sort/statistic-sort.component.spec.ts
@@ -103,7 +103,7 @@ describe('StatisticSortComponent', () => {
       'body',
       [],
       [],
-      [],
+      [1, 2, 3, 4],
       false,
       ContentType.SORT,
       new ContentState(1, new Date(), false)
@@ -113,5 +113,164 @@ describe('StatisticSortComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get correct top answer combinations if there are no correct answers', () => {
+    const combinations = [
+      {
+        count: 3,
+        selectedChoiceIndexes: [4, 3, 2, 1],
+      },
+      {
+        count: 4,
+        selectedChoiceIndexes: [1, 2, 4, 3],
+      },
+      {
+        count: 5,
+        selectedChoiceIndexes: [1, 3, 4, 2],
+      },
+      {
+        count: 6,
+        selectedChoiceIndexes: [1, 3, 2, 4],
+      },
+      {
+        count: 2,
+        selectedChoiceIndexes: [1, 4, 3, 2],
+      },
+      {
+        count: 2,
+        selectedChoiceIndexes: [1, 4, 2, 3],
+      },
+    ];
+    const topCombinations = component.getTopCombinations(combinations);
+    const correctTopCombinations = [
+      {
+        count: 6,
+        selectedChoiceIndexes: [1, 3, 2, 4],
+      },
+      {
+        count: 5,
+        selectedChoiceIndexes: [1, 3, 4, 2],
+      },
+      {
+        count: 4,
+        selectedChoiceIndexes: [1, 2, 4, 3],
+      },
+      {
+        count: 3,
+        selectedChoiceIndexes: [4, 3, 2, 1],
+      },
+      {
+        count: 4,
+        selectedChoiceIndexes: [],
+      },
+    ];
+    expect(topCombinations).toEqual(correctTopCombinations);
+  });
+
+  it('should get correct top answer combinations including correct one if there are correct answers even if they are sorted out', () => {
+    const combinations = [
+      {
+        count: 3,
+        selectedChoiceIndexes: [4, 3, 2, 1],
+      },
+      {
+        count: 4,
+        selectedChoiceIndexes: [1, 2, 4, 3],
+      },
+      {
+        count: 5,
+        selectedChoiceIndexes: [1, 3, 4, 2],
+      },
+      {
+        count: 6,
+        selectedChoiceIndexes: [1, 3, 2, 4],
+      },
+      {
+        count: 2,
+        selectedChoiceIndexes: [1, 2, 3, 4],
+      },
+      {
+        count: 2,
+        selectedChoiceIndexes: [1, 4, 2, 3],
+      },
+    ];
+    const topCombinations = component.getTopCombinations(combinations);
+    const correctTopCombinations = [
+      {
+        count: 6,
+        selectedChoiceIndexes: [1, 3, 2, 4],
+      },
+      {
+        count: 5,
+        selectedChoiceIndexes: [1, 3, 4, 2],
+      },
+      {
+        count: 4,
+        selectedChoiceIndexes: [1, 2, 4, 3],
+      },
+      {
+        count: 2,
+        selectedChoiceIndexes: [1, 2, 3, 4],
+      },
+      {
+        count: 5,
+        selectedChoiceIndexes: [],
+      },
+    ];
+    expect(topCombinations).toEqual(correctTopCombinations);
+  });
+
+  it('should get correct top answer combinations including correct one at its original position if there are correct answers if they are not sorted out', () => {
+    const combinations = [
+      {
+        count: 3,
+        selectedChoiceIndexes: [4, 3, 2, 1],
+      },
+      {
+        count: 4,
+        selectedChoiceIndexes: [1, 2, 3, 4],
+      },
+      {
+        count: 5,
+        selectedChoiceIndexes: [1, 3, 4, 2],
+      },
+      {
+        count: 6,
+        selectedChoiceIndexes: [1, 3, 2, 4],
+      },
+      {
+        count: 2,
+        selectedChoiceIndexes: [1, 2, 4, 3],
+      },
+      {
+        count: 2,
+        selectedChoiceIndexes: [1, 4, 2, 3],
+      },
+    ];
+    const topCombinations = component.getTopCombinations(combinations);
+    const correctTopCombinations = [
+      {
+        count: 6,
+        selectedChoiceIndexes: [1, 3, 2, 4],
+      },
+      {
+        count: 5,
+        selectedChoiceIndexes: [1, 3, 4, 2],
+      },
+      {
+        count: 4,
+        selectedChoiceIndexes: [1, 2, 3, 4],
+      },
+      {
+        count: 3,
+        selectedChoiceIndexes: [4, 3, 2, 1],
+      },
+      {
+        count: 4,
+        selectedChoiceIndexes: [],
+      },
+    ];
+    expect(topCombinations).toEqual(correctTopCombinations);
   });
 });
