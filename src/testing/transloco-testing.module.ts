b611c4f238f49b4d5b512855d44b9fe94f8d5fd1
Daniel Gerhardt <code@dgerhardt.net>
2023-09-05 15:29:44 +0200

    Add testing module for Transloco

---
+++ src/testing/transloco-testing.module.ts
@@ -0,0 +1,26 @@
+import {
+  TranslocoTestingModule,
+  TranslocoTestingOptions,
+} from '@ngneat/transloco';
+// import i18nEn from '../assets/i18n/en.json';
+// import i18nAdminEn from '../assets/i18n/admin/en.json';
+// import i18nCreatorEn from '../assets/i18n/creator/en.json';
+// import i18nParticipantEn from '../assets/i18n/participant/en.json';
+
+export function getTranslocoModule(options: TranslocoTestingOptions = {}) {
+  return TranslocoTestingModule.forRoot({
+    langs: {
+      // Uncomment the following lines to enable l10n strings for testing
+      // en: i18nEn,
+      // 'admin/en': i18nAdminEn,
+      // 'creator/en': i18nCreatorEn,
+      // 'participant/en': i18nParticipantEn,
+    },
+    translocoConfig: {
+      availableLangs: ['en'],
+      defaultLang: 'en',
+    },
+    preloadLangs: true,
+    ...options,
+  });
+}
