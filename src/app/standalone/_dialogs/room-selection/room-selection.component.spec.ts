91a4dfabdea42ac29e27ad652c941d5351dff538
Lukas Mauß <lukas.mauss@gmx.de>
2023-11-17 15:23:52 +0100

    Fix unit tests according to latest changes

--- src/app/standalone/_dialogs/room-selection/room-selection.component.spec.ts
+++ src/app/standalone/_dialogs/room-selection/room-selection.component.spec.ts
@@ -1,14 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RoomSelectionComponent } from './room-selection.component';
+import { RoomService } from '@app/core/services/http/room.service';
+import { RoomMembershipService } from '@app/core/services/room-membership.service';
+import { of } from 'rxjs';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MockMatDialogRef } from '@testing/test-helpers';
+import { getTranslocoModule } from '@testing/transloco-testing.module';
 
 describe('RoomSelectionComponent', () => {
   let component: RoomSelectionComponent;
   let fixture: ComponentFixture<RoomSelectionComponent>;
 
+  const mockRoomService = jasmine.createSpyObj(['getRoomSummaries']);
+
+  const mockRoomMembershipService = jasmine.createSpyObj(
+    RoomMembershipService,
+    ['getCurrentMemberships']
+  );
+  mockRoomMembershipService.getCurrentMemberships.and.returnValue(of([]));
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RoomSelectionComponent]
+      imports: [RoomSelectionComponent, getTranslocoModule()],
+      providers: [
+        {
+          provide: RoomService,
+          useValue: mockRoomService,
+        },
+        {
+          provide: RoomMembershipService,
+          useValue: mockRoomMembershipService,
+        },
+        {
+          provide: MatDialogRef,
+          useClass: MockMatDialogRef,
+        },
+      ],
     });
     fixture = TestBed.createComponent(RoomSelectionComponent);
     component = fixture.componentInstance;
--- src/app/standalone/content-group-template/content-group-template.component.spec.ts
+++ src/app/standalone/content-group-template/content-group-template.component.spec.ts
@@ -3,14 +3,49 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ContentGroupTemplateComponent } from './content-group-template.component';
 import { getTranslocoModule } from '@testing/transloco-testing.module';
 import { ContentGroupTemplate } from '@app/core/models/content-group-template';
+import { BaseTemplateService } from '@app/core/services/http/base-template.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NotificationService } from '@app/core/services/util/notification.service';
+import { MockNotificationService } from '@testing/test-helpers';
+import { RoutingService } from '@app/core/services/util/routing.service';
+import { RoomMembershipService } from '@app/core/services/room-membership.service';
 
 describe('ContentGroupTemplateComponent', () => {
   let component: ContentGroupTemplateComponent;
   let fixture: ComponentFixture<ContentGroupTemplateComponent>;
 
+  const mockBaseTemplateService = jasmine.createSpyObj(BaseTemplateService, [
+    'createCopyFromContentGroupTemplate',
+  ]);
+
+  const mockRoomMembershipService = jasmine.createSpyObj(['selectPrimaryRole']);
+
+  const mockRoutingService = jasmine.createSpyObj(RoutingService, [
+    'getRoleRoute',
+  ]);
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [ContentGroupTemplateComponent, getTranslocoModule()],
+      providers: [
+        {
+          provide: RoomMembershipService,
+          useValue: mockRoomMembershipService,
+        },
+        {
+          provide: BaseTemplateService,
+          useValue: mockBaseTemplateService,
+        },
+        {
+          provide: NotificationService,
+          useClass: MockNotificationService,
+        },
+        {
+          provide: RoutingService,
+          useValue: mockRoutingService,
+        },
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
     });
     fixture = TestBed.createComponent(ContentGroupTemplateComponent);
     component = fixture.componentInstance;
