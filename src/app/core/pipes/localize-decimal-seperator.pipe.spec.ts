d086fefad5546073f3c63188d62ffa8b802924b4
Lukas Mauß <lukas.mauss@gmx.de>
2024-01-23 11:18:47 +0100

    Create pipe for localizing decimal seperators

---
+++ src/app/core/pipes/localize-decimal-seperator.pipe.spec.ts
@@ -0,0 +1,72 @@
+import { LocalizeDecimalSeperatorPipe } from '@app/core/pipes/localize-decimal-seperator.pipe';
+
+import { TestBed } from '@angular/core/testing';
+
+import { TranslocoService } from '@ngneat/transloco';
+
+describe('LocalizeDecimalSeperatorPipe', () => {
+  const translateService = jasmine.createSpyObj('TranslocoService', [
+    'getActiveLang',
+  ]);
+  translateService.getActiveLang.and.returnValue('de');
+  let pipe: LocalizeDecimalSeperatorPipe;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: TranslocoService,
+          useValue: translateService,
+        },
+      ],
+    });
+    pipe = new LocalizeDecimalSeperatorPipe(translateService);
+  });
+
+  it('should be created', () => {
+    expect(pipe).toBeTruthy();
+  });
+
+  it('transforms "42" to "42"', () => {
+    expect(pipe.transform(42)).toBe('42');
+  });
+
+  it('transforms "42.00" to "42" with german localization', () => {
+    translateService.getActiveLang.and.returnValue('de');
+    expect(pipe.transform(42.0)).toBe('42');
+  });
+
+  it('transforms "42.42" to "42,42" with german localization', () => {
+    translateService.getActiveLang.and.returnValue('de');
+    expect(pipe.transform(42.42)).toBe('42,42');
+  });
+
+  it('transforms "42.4" to "42,4" with german localization', () => {
+    translateService.getActiveLang.and.returnValue('de');
+    expect(pipe.transform(42.4)).toBe('42,4');
+  });
+
+  it('transforms "42.42424242" to "42,42" with german localization', () => {
+    translateService.getActiveLang.and.returnValue('de');
+    expect(pipe.transform(42.42424242)).toBe('42,42');
+  });
+
+  it('transforms "42.00" to "42" with english localization', () => {
+    translateService.getActiveLang.and.returnValue('en');
+    expect(pipe.transform(42.0)).toBe('42');
+  });
+
+  it('transforms "42.42" to "42.42" with english localization', () => {
+    translateService.getActiveLang.and.returnValue('en');
+    expect(pipe.transform(42.42)).toBe('42.42');
+  });
+
+  it('transforms "42.4" to "42.4" with english localization', () => {
+    translateService.getActiveLang.and.returnValue('en');
+    expect(pipe.transform(42.4)).toBe('42.4');
+  });
+
+  it('transforms "42.42424242" to "42.42" with english localization', () => {
+    translateService.getActiveLang.and.returnValue('en');
+    expect(pipe.transform(42.42424242)).toBe('42.42');
+  });
+});
