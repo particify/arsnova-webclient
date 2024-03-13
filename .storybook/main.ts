ac9ff3bb715ed52634255457a606c18bb20cfc9f
Daniel Gerhardt <code@dgerhardt.net>
2024-03-13 10:31:54 +0000

    Migrate Storybook config for v8

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,13 @@
+import { dirname, join } from 'path';
 import type { StorybookConfig } from '@storybook/angular';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   staticDirs: [{ from: '../src/assets', to: '/assets' }],
-  addons: ['@storybook/addon-essentials'],
-  framework: '@storybook/angular',
+  addons: [getAbsolutePath('@storybook/addon-essentials')],
+  framework: getAbsolutePath('@storybook/angular'),
 };
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
