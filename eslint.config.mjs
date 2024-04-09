3c8dd65e3f6e59a8efb90b8316fb3f0eaa05087c
Daniel Gerhardt <code@dgerhardt.net>
2024-04-09 08:26:43 +0000

    Migrate ESLint config
    ESLint introduced a new flat config format back in 2022. The legacy
    config format has now been deprecated and support will be removed in the
    near future.
    
    See: https://eslint.org/docs/latest/use/configure/configuration-files

---
+++ eslint.config.mjs
@@ -0,0 +1,82 @@
+import eslint from '@eslint/js';
+import eslintTypescript from 'typescript-eslint';
+import eslintAngular from '@angular-eslint/eslint-plugin';
+import eslintAngularTemplate from '@angular-eslint/eslint-plugin-template';
+import eslintImport from 'eslint-plugin-import';
+import eslintStorybook from 'eslint-plugin-storybook';
+
+export default eslintTypescript.config(
+  eslint.configs.recommended,
+  ...eslintTypescript.configs.recommended,
+  {
+    ignores: ['**/*.d.ts', 'dist/**'],
+  },
+  {
+    plugins: {
+      '@angular-eslint': eslintAngular,
+      import: eslintImport,
+    },
+    languageOptions: {
+      parser: eslintTypescript.parser,
+      parserOptions: {
+        project: true,
+      },
+    },
+    rules: {
+      ...eslintAngular.configs.recommended.rules,
+      ...eslintAngularTemplate.configs['process-inline-templates'].rules,
+      ...eslintImport.configs.recommended.rules,
+      ...eslintImport.configs.typescript.rules,
+      complexity: ['error', 10],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: ['app', 'lib'],
+          style: 'kebab-case',
+        },
+      ],
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: ['app', 'lib'],
+          style: 'camelCase',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: ['../*'],
+        },
+      ],
+    },
+    settings: {
+      'import/resolver': {
+        typescript: true,
+      },
+    },
+  },
+  {
+    files: ['*.html'],
+    ...eslintAngularTemplate.configs.recommended,
+  },
+  {
+    files: ['*.spec.ts', 'test-helpers.ts'],
+    rules: {
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          args: 'none',
+        },
+      ],
+    },
+  },
+  {
+    files: ['*.stories.ts'],
+    ...eslintStorybook.configs.recommended,
+  }
+);
