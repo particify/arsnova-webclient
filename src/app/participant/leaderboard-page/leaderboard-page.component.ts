f2d89039de11212009ed2432343a92d1309cecb1
Lukas Mauß <lukas.mauss@gmx.de>
2024-06-11 10:17:22 +0200

    Refresh participants content leaderboard if there are incoming answers

--- src/app/participant/leaderboard-page/leaderboard-page.component.ts
+++ src/app/participant/leaderboard-page/leaderboard-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, DestroyRef, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Content } from '@app/core/models/content';
 import { CurrentLeaderboardItem } from '@app/core/models/current-leaderboard-item';
@@ -7,6 +7,9 @@ import { ContentLeaderboardComponent } from '@app/standalone/content-leaderboard
 import { LoadingIndicatorComponent } from '@app/standalone/loading-indicator/loading-indicator.component';
 import { NgIf } from '@angular/common';
 import { FlexModule } from '@angular/flex-layout';
+import { ContentService } from '@app/core/services/http/content.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { throttleTime } from 'rxjs';
 
 @Component({
   selector: 'app-leaderboard-page',
@@ -27,10 +30,22 @@ export class LeaderboardPageComponent implements OnInit {
 
   constructor(
     private contentGroupService: ContentGroupService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private contentService: ContentService,
+    private destroyRef: DestroyRef
   ) {}
 
   ngOnInit(): void {
+    this.loadLeaderboard();
+    this.contentService
+      .getAnswersChangedStream(this.content.roomId, this.content.id)
+      .pipe(takeUntilDestroyed(this.destroyRef), throttleTime(5000))
+      .subscribe(() => {
+        this.loadLeaderboard();
+      });
+  }
+
+  private loadLeaderboard(): void {
     this.contentGroupService
       .getCurrentLeaderboard(
         this.route.snapshot.data.room.id,
