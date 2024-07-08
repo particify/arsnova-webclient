b6c95bb11488151abfb3ae961f88f415d1780248
Lukas Mauß <lukas.mauss@gmx.de>
2024-07-08 12:20:46 +0200

    Improve content group leaderboard
    * Use table for displaying leaderboard
    * Center entry position in fixed size cell
    * Limit list to top ten entries but add column for own entry if not listed

--- src/app/standalone/leaderboard/leaderboard.component.ts
+++ src/app/standalone/leaderboard/leaderboard.component.ts
@@ -1,16 +1,54 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { CoreModule } from '@app/core/core.module';
 import { LeaderboardItem } from '@app/core/models/leaderboard-item';
+import { RoomUserAlias } from '@app/core/models/room-user-alias';
 import { OrdinalPipe } from '@app/core/pipes/ordinal.pipe';
+import { MatTableDataSource } from '@angular/material/table';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+interface LeaderboardTableItem {
+  position: number;
+  userAlias: RoomUserAlias;
+  score: number;
+}
 
 @Component({
   standalone: true,
-  imports: [CoreModule, OrdinalPipe],
+  imports: [CoreModule, OrdinalPipe, FlexLayoutModule],
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.component.html',
   styleUrl: './leaderboard.component.scss',
 })
-export class LeaderboardComponent {
+export class LeaderboardComponent implements OnChanges {
   @Input({ required: true }) leaderboardItems: LeaderboardItem[] = [];
   @Input() aliasId?: string;
+
+  dataSource?: MatTableDataSource<LeaderboardTableItem>;
+
+  ngOnChanges(): void {
+    const tableItems: LeaderboardTableItem[] = [];
+    this.leaderboardItems.forEach((item, index) => {
+      if (
+        index < 10 ||
+        (this.showBelowList() && item.userAlias.id === this.aliasId)
+      ) {
+        tableItems.push({
+          position: index + 1,
+          userAlias: item.userAlias,
+          score: item.score,
+        });
+      }
+    });
+    this.dataSource = new MatTableDataSource(tableItems);
+  }
+
+  private showBelowList(): boolean {
+    if (this.aliasId) {
+      return !this.leaderboardItems
+        .slice(0, 10)
+        .map((l) => l.userAlias.id)
+        .includes(this.aliasId);
+    }
+    return false;
+  }
 }
