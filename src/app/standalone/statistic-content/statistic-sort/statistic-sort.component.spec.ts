1461394cf31ab8507b226b29f2756995c94a4759
Lukas Mauß <lukas.mauss@gmx.de>
2024-11-07 16:56:16 +0100

    Fix detection of correct sort answers and add unit tests
    Currently if there are more than four answers, but no correct one, a type error is thrown.

--- src/app/standalone/statistic-content/statistic-sort/statistic-sort.component.spec.ts
+++ src/app/standalone/statistic-content/statistic-sort/statistic-sort.component.spec.ts
@@ -12,6 +12,7 @@ import { AnswerStatistics } from '@app/core/models/answer-statistics';
 import { PresentationService } from '@app/core/services/util/presentation.service';
 import { EventEmitter, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ContentAnswerService } from '@app/core/services/http/content-answer.service';
+import { AnswerOption } from '@app/core/models/answer-option';
 
 describe('StatisticSortComponent', () => {
   let component: StatisticSortComponent;
@@ -77,7 +78,12 @@ describe('StatisticSortComponent', () => {
       'subject',
       'body',
       [],
-      [],
+      [
+        new AnswerOption('a'),
+        new AnswerOption('b'),
+        new AnswerOption('c'),
+        new AnswerOption('d'),
+      ],
       [1, 2, 3, 4],
       false,
       ContentType.SORT
@@ -247,5 +253,141 @@ describe('StatisticSortComponent', () => {
       },
     ];
     expect(topCombinations).toEqual(correctTopCombinations);
+  });
+
+  it('should detect that combination is wrong', () => {
+    const combination = { selectedChoiceIndexes: [3, 2, 1, 0], count: 1 };
+    expect(component.isCombinationCorrect(combination)).toEqual(false);
+  });
+
+  it('should detect that combination is correct', () => {
+    const combination = { selectedChoiceIndexes: [0, 1, 2, 3], count: 1 };
+    expect(component.isCombinationCorrect(combination)).toEqual(true);
+  });
+
+  it('should detect that combination index is wrong for single answer', () => {
+    component.stats = {
+      round: 1,
+      answerCount: 1,
+      abstentionCount: 0,
+      independentCounts: [1, 1, 1, 1],
+      combinatedCounts: [{ selectedChoiceIndexes: [3, 2, 1, 0], count: 1 }],
+    };
+    fixture.detectChanges();
+    expect(component.isCombinationIndexCorrect(0)).toEqual(false);
+  });
+
+  it('should detect that combination index is correct for single answer', () => {
+    component.stats = {
+      round: 1,
+      answerCount: 1,
+      abstentionCount: 0,
+      independentCounts: [1, 1, 1, 1],
+      combinatedCounts: [{ selectedChoiceIndexes: [0, 1, 2, 3], count: 1 }],
+    };
+    fixture.detectChanges();
+    expect(component.isCombinationIndexCorrect(0)).toEqual(true);
+  });
+
+  it('should get correct answer counts corretly if there are no answers', () => {
+    component.stats = {
+      round: 1,
+      answerCount: 0,
+      abstentionCount: 0,
+      independentCounts: [0, 0, 0, 0],
+      combinatedCounts: [],
+    };
+    fixture.detectChanges();
+    expect(component.getCorrectAnswerCounts()).toEqual(undefined);
+  });
+
+  it('should get correct answer counts correctly if there is only one wrong answer', () => {
+    component.stats = {
+      round: 1,
+      answerCount: 1,
+      abstentionCount: 0,
+      independentCounts: [1, 1, 1, 1],
+      combinatedCounts: [{ selectedChoiceIndexes: [3, 2, 1, 0], count: 1 }],
+    };
+    fixture.detectChanges();
+    expect(component.getCorrectAnswerCounts()).toEqual([0]);
+  });
+
+  it('should get correct answer counts correctly if there are 4 wrong answers', () => {
+    component.stats = {
+      round: 1,
+      answerCount: 20,
+      abstentionCount: 0,
+      independentCounts: [20, 20, 20, 20],
+      combinatedCounts: [
+        { selectedChoiceIndexes: [3, 2, 1, 0], count: 1 },
+        { selectedChoiceIndexes: [1, 3, 0, 2], count: 1 },
+        { selectedChoiceIndexes: [0, 3, 2, 1], count: 1 },
+        { selectedChoiceIndexes: [0, 2, 3, 1], count: 1 },
+      ],
+    };
+    fixture.detectChanges();
+    expect(component.getCorrectAnswerCounts()).toEqual([0]);
+  });
+
+  it('should get correct answer counts correctly if there are only multiple wrong answers', () => {
+    component.stats = {
+      round: 1,
+      answerCount: 20,
+      abstentionCount: 0,
+      independentCounts: [20, 20, 20, 20],
+      combinatedCounts: [
+        { selectedChoiceIndexes: [3, 2, 1, 0], count: 1 },
+        { selectedChoiceIndexes: [1, 3, 0, 2], count: 1 },
+        { selectedChoiceIndexes: [0, 3, 2, 1], count: 1 },
+        { selectedChoiceIndexes: [0, 2, 3, 1], count: 1 },
+        { selectedChoiceIndexes: [1, 0, 3, 2], count: 5 },
+        { selectedChoiceIndexes: [2, 0, 3, 1], count: 1 },
+        { selectedChoiceIndexes: [3, 1, 2, 0], count: 1 },
+        { selectedChoiceIndexes: [0, 3, 1, 2], count: 1 },
+        { selectedChoiceIndexes: [3, 0, 2, 1], count: 2 },
+        { selectedChoiceIndexes: [3, 0, 1, 2], count: 2 },
+        { selectedChoiceIndexes: [3, 1, 0, 2], count: 4 },
+      ],
+    };
+    fixture.detectChanges();
+    expect(component.getCorrectAnswerCounts()).toEqual([0]);
+  });
+
+  it('should get correct answer counts correctly if there is only one correct answer', () => {
+    component.stats = {
+      round: 1,
+      answerCount: 1,
+      abstentionCount: 0,
+      independentCounts: [1, 1, 1, 1],
+      combinatedCounts: [{ selectedChoiceIndexes: [0, 1, 2, 3], count: 1 }],
+    };
+    fixture.detectChanges();
+    expect(component.getCorrectAnswerCounts()).toEqual([1]);
+  });
+
+  it('should get correct answer counts correctly if there are different answers with also correct ones', () => {
+    component.stats = {
+      round: 1,
+      answerCount: 25,
+      abstentionCount: 0,
+      independentCounts: [25, 25, 25, 25],
+      combinatedCounts: [
+        { selectedChoiceIndexes: [3, 2, 1, 0], count: 1 },
+        { selectedChoiceIndexes: [1, 3, 0, 2], count: 1 },
+        { selectedChoiceIndexes: [0, 3, 2, 1], count: 1 },
+        { selectedChoiceIndexes: [0, 2, 3, 1], count: 1 },
+        { selectedChoiceIndexes: [1, 0, 3, 2], count: 5 },
+        { selectedChoiceIndexes: [2, 0, 3, 1], count: 1 },
+        { selectedChoiceIndexes: [3, 1, 2, 0], count: 1 },
+        { selectedChoiceIndexes: [0, 3, 1, 2], count: 1 },
+        { selectedChoiceIndexes: [3, 0, 2, 1], count: 2 },
+        { selectedChoiceIndexes: [3, 0, 1, 2], count: 2 },
+        { selectedChoiceIndexes: [3, 1, 0, 2], count: 4 },
+        { selectedChoiceIndexes: [0, 1, 2, 3], count: 5 },
+      ],
+    };
+    fixture.detectChanges();
+    expect(component.getCorrectAnswerCounts()).toEqual([5]);
   });
 });
