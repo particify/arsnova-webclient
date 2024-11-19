7deec8839200d11f48b28d518580a0b14af38fcf
Lukas Mauß <lukas.mauss@gmx.de>
2024-11-19 09:49:16 +0100

    Setup E2E tests with Playwright
    * Install playwright
    * Define default config for tests
    * Add Playwright script to package.json
    * Update gitignore to exclude Playwright results
    * Add Playwright extension to devcontainer config

---
+++ e2e/playwright.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: false,
+  retries: 0,
+  workers: undefined,
+  reporter: 'html',
+  use: {
+    baseURL: 'http://localhost:4200',
+    browserName: 'chromium',
+    trace: 'on',
+    headless: false,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+  ],
+});
