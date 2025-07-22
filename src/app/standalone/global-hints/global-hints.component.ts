c2ea0066f9aff59fef4818c847605c519d783270
Lukas Weingarten <lw_git@particify.de>
2025-07-22 10:33:37 +0200

    Add extension point for tenant-info
    Co-authored-by: Daniel Gerhardt <dg_git@particify.de>

--- src/app/standalone/global-hints/global-hints.component.ts
+++ src/app/standalone/global-hints/global-hints.component.ts
@@ -3,13 +3,14 @@ import { toSignal } from '@angular/core/rxjs-interop';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ApiConfigService } from '@app/core/services/http/api-config.service';
 import { HintComponent } from '@app/standalone/hint/hint.component';
+import { ExtensionPointComponent } from '@projects/extension-point/src/public-api';
 import { map } from 'rxjs';
 
 @Component({
   selector: 'app-global-hints',
   templateUrl: './global-hints.component.html',
   styleUrl: './global-hints.component.scss',
-  imports: [HintComponent, FlexLayoutModule],
+  imports: [HintComponent, ExtensionPointComponent, FlexLayoutModule],
 })
 export class GlobalHintsComponent {
   private apiConfigService = inject(ApiConfigService);
