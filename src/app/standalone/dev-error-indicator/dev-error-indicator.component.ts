b6d05f036d7a6cecdc8ee7f79531cd82b66ca1e7
Daniel Gerhardt <dg_git@particify.de>
2025-07-28 13:21:10 +0000

    Only use DevErrorIndicator for UI errors
    This also adds a tooltip to the icon.

--- src/app/standalone/dev-error-indicator/dev-error-indicator.component.ts
+++ src/app/standalone/dev-error-indicator/dev-error-indicator.component.ts
@@ -1,23 +1,21 @@
-import { Component, computed, ErrorHandler, inject } from '@angular/core';
+import { Component, ErrorHandler, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { MatBadge } from '@angular/material/badge';
 import { MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { MatTooltip } from '@angular/material/tooltip';
 import { AppErrorHandler } from '@app/app-error-handler';
 
 @Component({
   selector: 'app-dev-error-indicator',
   templateUrl: './dev-error-indicator.component.html',
   styleUrl: './dev-error-indicator.component.scss',
-  imports: [MatBadge, MatIcon, MatIconButton],
+  imports: [MatBadge, MatIcon, MatIconButton, MatTooltip],
 })
 export class DevErrorIndicatorComponent {
   private errorHandler = inject(ErrorHandler) as AppErrorHandler;
   private uiErrorCount$ = this.errorHandler.uiErrorCount$;
-  private httpErrorCount$ = this.errorHandler.httpErrorCount$;
   uiErrorCount = toSignal(this.uiErrorCount$, { initialValue: 0 });
-  httpErrorCount = toSignal(this.httpErrorCount$, { initialValue: 0 });
-  errorCount = computed(() => this.uiErrorCount() + this.httpErrorCount());
 
   reset() {
     this.errorHandler.reset();
