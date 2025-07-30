cd8c9ec5773ccbf8bd9f8a3440d6cefac0c08b44
Lukas Weingarten <lw_git@particify.de>
2025-07-30 10:57:53 +0200

    Add dayjs imports for spanish and italian localization

--- src/app/core/pipes/date-from-now.pipe.ts
+++ src/app/core/pipes/date-from-now.pipe.ts
@@ -2,7 +2,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/de';
-
+import 'dayjs/locale/es';
+import 'dayjs/locale/it';
 @Pipe({ name: 'dateFromNow' })
 export class DateFromNowPipe implements PipeTransform {
   /* The refresh parameter is not used but forces rerendering when changed. */
